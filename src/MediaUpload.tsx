6334b4b6084ac6a739e05206155c435fc216055a
ubiquifier <ubiquify@proton.me>
2023-10-03 09:01:05 -0400

    Fix svg warning

--- src/MediaUpload.tsx
+++ src/MediaUpload.tsx
@@ -197,7 +197,7 @@ const MediaUpload: React.FC<MediaUploadProps> = ({
     icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        enable-background="new 0 0 24 24"
+        enableBackground="new 0 0 24 24"
         width="24"
         viewBox="0 0 24 24"
       >
